0e03ab5920377f98e2195211ebf57f66df9fd045
Baburao Parab <Baburao.Parab@ibm.com>
2020-10-16 23:28:10 +0530

    Updates

---
+++ precache-manifest.35be92c0283397c0ed03316aee67d4d0.js
@@ -0,0 +1,182 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a7f9ea06221b42733af8a0d805b742c",
+    "url": "/kinectic/index.html"
+  },
+  {
+    "revision": "02397a3b6e901ed5f178",
+    "url": "/kinectic/static/css/main.a970c822.chunk.css"
+  },
+  {
+    "revision": "f2b56f0c98f17cec2d70",
+    "url": "/kinectic/static/js/0.527831b4.chunk.js"
+  },
+  {
+    "revision": "61c47307a5d4ec987ec7",
+    "url": "/kinectic/static/js/3.678c417f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/kinectic/static/js/3.678c417f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0828e35101c73a95f261",
+    "url": "/kinectic/static/js/4.f425181a.chunk.js"
+  },
+  {
+    "revision": "b5139974958b1e111b1a",
+    "url": "/kinectic/static/js/5.1c10526a.chunk.js"
+  },
+  {
+    "revision": "c64509a547b680875b7f",
+    "url": "/kinectic/static/js/6.6afb4a30.chunk.js"
+  },
+  {
+    "revision": "ad7c393ee4a35ec14967",
+    "url": "/kinectic/static/js/7.97696ee2.chunk.js"
+  },
+  {
+    "revision": "aed727ff23c5e432e9ec",
+    "url": "/kinectic/static/js/8.92f570ac.chunk.js"
+  },
+  {
+    "revision": "02397a3b6e901ed5f178",
+    "url": "/kinectic/static/js/main.2a85a016.chunk.js"
+  },
+  {
+    "revision": "3ced41c6a6a8995da4d3",
+    "url": "/kinectic/static/js/runtime-main.80f951da.js"
+  },
+  {
+    "revision": "1a2d534612252707ad81fe7013f090ac",
+    "url": "/kinectic/static/media/about-us.1a2d5346.jpg"
+  },
+  {
+    "revision": "cfd789e5d117e316c8179a0566492479",
+    "url": "/kinectic/static/media/antenna-isro.cfd789e5.jpg"
+  },
+  {
+    "revision": "bb450d4c421332311df23678a284ae75",
+    "url": "/kinectic/static/media/bae-poseidon.bb450d4c.jpg"
+  },
+  {
+    "revision": "8d801d8155cc1d3e3c2b5bc3e7e17a30",
+    "url": "/kinectic/static/media/kineco-kaman-logo.8d801d81.png"
+  },
+  {
+    "revision": "84d7456435d13599151ecae5dae076e2",
+    "url": "/kinectic/static/media/kineco-kaman-videos.84d74564.jpg"
+  },
+  {
+    "revision": "051c8e17ac9a158705d2a7c39661e9b7",
+    "url": "/kinectic/static/media/leadership-team.051c8e17.jpg"
+  },
+  {
+    "revision": "8b2f1dc6c705f9ecaf35a07390cf2bb7",
+    "url": "/kinectic/static/media/news-video-bg.8b2f1dc6.jpg"
+  },
+  {
+    "revision": "f58366fc2e25fb139894bb05e4c07317",
+    "url": "/kinectic/static/media/product-01.f58366fc.jpg"
+  },
+  {
+    "revision": "cf704450fd94d704776056e1bac43d6e",
+    "url": "/kinectic/static/media/product-02.cf704450.jpg"
+  },
+  {
+    "revision": "74d3db652966444dd28ae284368bec6b",
+    "url": "/kinectic/static/media/product-03.74d3db65.jpg"
+  },
+  {
+    "revision": "d2d96a966833d7cc8d6d3f0d542b98d6",
+    "url": "/kinectic/static/media/product-04.d2d96a96.jpg"
+  },
+  {
+    "revision": "e30a1eb738a03b843450b0ade07e86fb",
+    "url": "/kinectic/static/media/product-05.e30a1eb7.jpg"
+  },
+  {
+    "revision": "71f6aaa46b44c67381f2e8afa3b2b296",
+    "url": "/kinectic/static/media/product-06.71f6aaa4.jpg"
+  },
+  {
+    "revision": "2b054912a18afc86cd95ea46f878d836",
+    "url": "/kinectic/static/media/product-07.2b054912.jpg"
+  },
+  {
+    "revision": "67307d6aa5e2425af8fef0c7810cb193",
+    "url": "/kinectic/static/media/product-08.67307d6a.jpg"
+  },
+  {
+    "revision": "9220cef4fc48f20306674fc33a84aed9",
+    "url": "/kinectic/static/media/product-09.9220cef4.jpg"
+  },
+  {
+    "revision": "ebdff4e2e8248d566271f209f95e3190",
+    "url": "/kinectic/static/media/product-10.ebdff4e2.jpg"
+  },
+  {
+    "revision": "cb13afd0e2442408708e6c3ca2b4724c",
+    "url": "/kinectic/static/media/slider-about-us-02.cb13afd0.jpg"
+  },
+  {
+    "revision": "03f897dde915faabbb64c168eb9c3998",
+    "url": "/kinectic/static/media/slider-about-us-03.03f897dd.jpg"
+  },
+  {
+    "revision": "ed9685445c3d78f50d5e120569bbab4c",
+    "url": "/kinectic/static/media/slider-about-us-04.ed968544.jpg"
+  },
+  {
+    "revision": "922a9cc5695309b362e6ceb69204b31d",
+    "url": "/kinectic/static/media/slider-about-us-05.922a9cc5.jpg"
+  },
+  {
+    "revision": "3ce5bff637951d5518fad41824258c47",
+    "url": "/kinectic/static/media/slider-home-01.3ce5bff6.jpg"
+  },
+  {
+    "revision": "209cff1ddbf0f15b09064375785edcee",
+    "url": "/kinectic/static/media/slider-home-02.209cff1d.jpg"
+  },
+  {
+    "revision": "5e8848b3923ac306f9e1a01aa19c9505",
+    "url": "/kinectic/static/media/slider-home-03.5e8848b3.jpg"
+  },
+  {
+    "revision": "634bd20dc1bc8a42e5a012a977b82d87",
+    "url": "/kinectic/static/media/slider-home-04.634bd20d.jpg"
+  },
+  {
+    "revision": "52a3d1ff64f365030690096ea90baf97",
+    "url": "/kinectic/static/media/slider-home-05.52a3d1ff.jpg"
+  },
+  {
+    "revision": "5a2a2cd8873c8d67a46af71989a6d684",
+    "url": "/kinectic/static/media/slider-home-06.5a2a2cd8.jpg"
+  },
+  {
+    "revision": "b1a0d92c6d665e27f8e364369050a605",
+    "url": "/kinectic/static/media/slider-news-01.b1a0d92c.jpg"
+  },
+  {
+    "revision": "76e017887c1924dbb09a5a446402c00b",
+    "url": "/kinectic/static/media/slider-news-02.76e01788.jpg"
+  },
+  {
+    "revision": "c1992bf45ea0ab8660a18f9078aa920c",
+    "url": "/kinectic/static/media/slider-products-01.c1992bf4.jpg"
+  },
+  {
+    "revision": "d432f196dd96f0feeeb2663e566b9e6b",
+    "url": "/kinectic/static/media/slider-products-02.d432f196.jpg"
+  },
+  {
+    "revision": "ea748cecb86305d2795719a688579581",
+    "url": "/kinectic/static/media/slider-products-03.ea748cec.jpg"
+  },
+  {
+    "revision": "08aabad8827fe01d9d757e56eb6c6623",
+    "url": "/kinectic/static/media/slider-products-04.08aabad8.jpg"
+  }
+]);
